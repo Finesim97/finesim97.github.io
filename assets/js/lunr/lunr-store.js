70264e3f9155ab182cb2302186cc0f3799a28f5d
Travis CI User <travis@example.org>
2019-02-17 15:31:02 +0000

    new version Sun Feb 17 15:31:02 UTC 2019

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,11 @@
-var store = []
+var store = [{
+        "title": "Hello (maybe simulated) World!",
+        "excerpt":"This year marks the start of my personal website and blog. So: Happy new Year! Why am I doing this? The time of the blog may be over. I have to say, at the moment of writing this post I am not actively following any blog myself, so the question...","categories": ["Personal"],
+        "tags": ["Project Start","Project Finish"],
+        "url": "https://www.ljansen.net/personal/hello-world",
+        "teaser":null},{
+        "title": "Blog Setup with Jekyll",
+        "excerpt":"I thought the first project description should be my automatic jekyll static page generation. Why tho? At least 20% of the popular internet runs on WordPress. [1] Some years ago I hosted a WordPress blog on my server. It is really comfortable and configurable, the need for PHP and a...","categories": ["Projects"],
+        "tags": ["Project Finish","Website"],
+        "url": "https://www.ljansen.net/projects/jekyll-setup",
+        "teaser":null}]
