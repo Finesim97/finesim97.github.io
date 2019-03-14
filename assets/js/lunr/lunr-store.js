a0eb455cf36fce2ada510c923224b0309eaadbed
Travis CI User <travis@example.org>
2019-03-14 17:17:20 +0000

    new version Thu Mar 14 17:17:20 UTC 2019

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,11 +1,11 @@
 var store = [{
         "title": "Hello (maybe simulated) World!",
-        "excerpt":"This year marks the start of my personal website and blog. So: Happy new Year! Why am I doing this? The time of the blog may be over. I have to say, at the moment of writing this post I am not actively following any blog myself, so the question...","categories": ["Personal"],
+        "excerpt":"This year marks the start of my personal website and blog. So: Happy new Year! Why am I doing this? The time of the blog may be over. I have to say, at the moment of writing this post, I am not actively following any blog myself, so the question...","categories": ["Personal"],
         "tags": ["Project Start","Project Finish"],
-        "url": "https://www.ljansen.net/personal/hello-world",
+        "url": "https://www.lukas-jansen.de/personal/hello-world",
         "teaser":null},{
         "title": "Blog Setup with Jekyll",
-        "excerpt":"I thought the first project description should be my automatic jekyll static page generation. Why tho? At least 20% of the popular internet runs on WordPress. [1] Some years ago I hosted a WordPress blog on my server. It is really comfortable and configurable, the need for PHP and a...","categories": ["Projects"],
+        "excerpt":"I thought the first project description should be my automatic jekyll page generation. Why tho? At least 20% of the popular internet runs on WordPress. [1] Some years ago I hosted a WordPress blog on my server. It is really comfortable and configurable, the need for PHP and a DBS...","categories": ["Projects"],
         "tags": ["Project Finish","Website"],
-        "url": "https://www.ljansen.net/projects/jekyll-setup",
+        "url": "https://www.lukas-jansen.de/projects/jekyll-setup",
         "teaser":null}]
